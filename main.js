72b7e2a8e5866dd3b7f013456007fe4c73caa03b
maliacho <maliacho21@gmail.com>
2024-01-16 11:05:34 -0800

    redesigned landing page

---
+++ main.js
@@ -0,0 +1,16 @@
+// Smooth Scroll Option 2
+
+// $('.navbar a').on('click', function(e) { 
+//     if(this.hash !== '') {
+//         e.preventDefault();
+//         const hash = this.hash;
+//         $('html, body').animate({
+//             scrollTop: $(hash).offset().top
+//         }, 800);
+//     }
+// });
+
+// Smooth Scroll Option 3
+const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+    speed: 800
+});
